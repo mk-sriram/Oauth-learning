72941c8033ccd136da1c821fbca6437abcae3003
Sriram MK <mksriram@umich.edu>
2024-02-29 00:49:49 -0500

    learnt to install Google login button

---
+++ fumblecounter/src/components/login.js
@@ -0,0 +1,9 @@
+import {GoogleLogin } from 'react-google-login'; 
+const clientID = "938760873544-79vlv3jsee0bqvprhqcf7boe38q0gg2t.apps.googleusercontent.com"; 
+
+
+const login = () => { 
+    return ( 
+        
+    )
+}
